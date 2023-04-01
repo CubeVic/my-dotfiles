a3e495102c9d7ead381a89b773d52113cc8b0b2a
Victor Andres <victor.andres@opennet.tw>
2023-04-01 21:12:56 +0800

    feat: adding script to take notes

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -2,3 +2,12 @@
 #
 
 alias path-vars='zsh $HOME/.dotfiles/zsh/scripts/path-vars.zsh'
+n() {
+    nf="$HOME/Desktop/scratch.md"
+
+    dates=$(date +'%n## %Y-%m-%d %H:%M:%S')
+    echo "$dates" >> $nf
+    echo -e  ">\n " >> $nf
+    vim + -c :$o $nf
+
+}
